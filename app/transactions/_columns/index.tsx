d44261e1387cabba3138e643966bf1244350ec73
gustavoking <gustavorostirollax@gmail.com>
2025-05-27 12:04:39 -0300

    feat: add badge shadcn

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Transaction, TransactionType } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => {
+      if (transaction.type === TransactionType.DEPOSIT) {
+        return "Depósito";
+      }
+    },
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
