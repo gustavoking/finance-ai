c910c7a6974b94520f160e643078330589314688
gustavoking <gustavorostirollax@gmail.com>
2025-05-28 11:28:33 -0300

    feat: add config danger and add badge

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -2,6 +2,8 @@
 
 import { Transaction, TransactionType } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import { Badge } from "@/app/_components/ui/badge";
+import { CircleIcon } from "lucide-react";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -13,10 +15,30 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
     header: "Tipo",
     cell: ({ row: { original: transaction } }) => {
       if (transaction.type === TransactionType.DEPOSIT) {
-        return "Depósito";
+        return (
+          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+            <CircleIcon className="mr-2 fill-primary" size={10} />
+            Crédito
+          </Badge>
+        );
       }
+      if (transaction.type === TransactionType.EXPENSE) {
+        return (
+          <Badge className="bg-danger/10 font-bold text-danger hover:bg-danger/10">
+            <CircleIcon className="mr-2 fill-danger" size={10} />
+            Débito
+          </Badge>
+        );
+      }
+      return (
+        <Badge className="font bold bg-white/10 text-white hover:bg-white/10">
+          <CircleIcon className="mr-2 fill-white" size={10} />
+          Investimento
+        </Badge>
+      );
     },
   },
+
   {
     accessorKey: "category",
     header: "Categoria",
