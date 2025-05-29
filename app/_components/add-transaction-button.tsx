1474acdb16623a17ccb8777e66212221fd4087ce
gustavoking <gustavorostirollax@gmail.com>
2025-05-29 00:29:32 -0300

    feat: add label, react-hook-form and zod and forSchema

--- app/_components/add-transaction-button.tsx
+++ app/_components/add-transaction-button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ArrowDownUp } from "lucide-react";
 import { Button } from "./ui/button";
 import {
@@ -10,7 +12,64 @@ import {
   DialogTrigger,
 } from "./ui/dialog";
 
+import { z } from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+
+import { Input } from "./ui/input";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "o nome é obrigatório",
+  }),
+  amount: z.string().trim().min(1, {
+    message: "o valor é obrigatorio",
+  }),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatório",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "A forma de pagamento é obrigatória",
+  }),
+  date: z.date({
+    required_error: "A data é obrigatória",
+  }),
+});
+
 const AddTransactionButton = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: "",
+      category: TransactionCategory.OTHER,
+      date: new Date(),
+      name: "",
+      paymentMethod: TransactionPaymentMethod.PIX,
+      type: TransactionType.EXPENSE,
+    },
+  });
+
+  const onSubmit = () => {};
+
   return (
     <div>
       <Dialog>
@@ -26,10 +85,32 @@ const AddTransactionButton = () => {
             <DialogDescription>Insira as informações abaixo</DialogDescription>
           </DialogHeader>
 
-          <DialogFooter>
-            <Button variant="outline">Cancelar</Button>
-            <Button className="hover:bg-green-800">Adicionar</Button>
-          </DialogFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Histórico da Transação..."
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <DialogFooter>
+                <Button variant="outline">Cancelar</Button>
+                <Button className="hover:bg-green-800">Adicionar</Button>
+              </DialogFooter>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
