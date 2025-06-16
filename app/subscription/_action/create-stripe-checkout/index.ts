e8b7adb3affc5a8d645b5238ddfb13a484a90e8d
gustavoking <gustavorostirollax@gmail.com>
2025-06-16 20:00:54 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/_action/create-stripe-checkout/index.ts
+++ app/subscription/_action/create-stripe-checkout/index.ts
@@ -5,19 +5,17 @@ import Stripe from "stripe";
 
 export const createStripeCheckout = async () => {
   const { userId } = await auth();
-
   if (!userId) {
-    throw new Error("Unathorized");
+    throw new Error("Unauthorized");
   }
   if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Stripe key was not found");
+    throw new Error("Stripe secret key not found");
   }
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
   });
-
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card", "boleto"],
+    payment_method_types: ["card"],
     mode: "subscription",
     success_url: "http://localhost:3000",
     cancel_url: "http://localhost:3000",
