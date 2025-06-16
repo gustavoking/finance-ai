e306aef22a437880e8505b887cec6aeffffc1037
gustavoking <gustavorostirollax@gmail.com>
2025-06-16 20:32:20 -0300

    feat: ensure user can not create transaction on free plan if above limit

--- app/(home)/_components/summary-card.tsx
+++ app/(home)/_components/summary-card.tsx
@@ -7,6 +7,7 @@ interface SummaryCardProps {
   title: string;
   amount: number;
   size?: "small" | "large";
+  userCanAddTransaction?: boolean;
 }
 
 const SummaryCard = ({
@@ -14,32 +15,33 @@ const SummaryCard = ({
   title,
   amount,
   size = "small",
+  userCanAddTransaction,
 }: SummaryCardProps) => {
   return (
-    <>
-      <Card className={`${size === "large" ? "bg-white/5" : ""}`}>
-        <CardHeader className="flex-row items-center gap-4">
-          {icon}
-          <p
-            className={`${size === "small" ? "text-muted-foreground" : "text-white opacity-70"}`}
-          >
-            {title}
-          </p>
-        </CardHeader>
-        <CardContent className="flex justify-between">
-          <p
-            className={`font-bold ${size === "small" ? "text-2xl" : "text-4xl"}`}
-          >
-            {Intl.NumberFormat("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }).format(amount)}
-          </p>
+    <Card>
+      <CardHeader className="flex-row items-center gap-4">
+        {icon}
+        <p
+          className={`${size === "small" ? "text-muted-foreground" : "text-white opacity-70"}`}
+        >
+          {title}
+        </p>
+      </CardHeader>
+      <CardContent className="flex justify-between">
+        <p
+          className={`font-bold ${size === "small" ? "text-2xl" : "text-4xl"}`}
+        >
+          {Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(amount)}
+        </p>
 
-          {size === "large" && <AddTransactionButton />}
-        </CardContent>
-      </Card>
-    </>
+        {size === "large" && (
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
+        )}
+      </CardContent>
+    </Card>
   );
 };
 
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -6,6 +6,7 @@ import NavBar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
   const { userId } = await auth();
@@ -15,6 +16,8 @@ const TransactionsPage = async () => {
   }
 
   const transactions = await db.transaction.findMany({ where: { userId } });
+
+  const canUserAddTransactions = await canUserAddTransaction();
   return (
     <>
       <NavBar />
@@ -22,7 +25,9 @@ const TransactionsPage = async () => {
         {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
-          <AddTransactionButton />
+          <AddTransactionButton
+            userCanAddTransaction={canUserAddTransactions}
+          />
         </div>
         <ScrollArea>
           <DataTable columns={transactionColumns} data={transactions} />
